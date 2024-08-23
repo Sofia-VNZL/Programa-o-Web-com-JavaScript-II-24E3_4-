574c5ba6ebefbf0137e1a857639731318ff6ed78
sofia <sofiaelena08200@gmail.com>
2024-08-22 21:45:41 -0300

    Exibição da quantidade de planetas

--- Semana 03/script.js
+++ Semana 03/script.js
@@ -6,4 +6,10 @@ planetas.push("Jupiter");
 planetas.push("Marte");
 planetas.push("Terra");
 
-alert("Planetas: " + planetas)
+alert("Tamanho do array: " + planetas.length);
+
+planetas[5] = "Saturno";
+
+alert("Tamanho do array: " + planetas.length);
+
+alert("Planetas: " + planetas);
