642116393370ed019d66848684234394c7826af9
sofia <sofiaelena08200@gmail.com>
2024-08-22 21:12:05 -0300

    Adição de vetor de planetas

---
+++ Semana 03/script.js
@@ -0,0 +1,9 @@
+let planetas = []
+
+planetas.push("Mercúrio");
+planetas.push("Venus");
+planetas.push("Jupiter");
+planetas.push("Marte");
+planetas.push("Terra");
+
+alert("Planetas: " + planetas)
